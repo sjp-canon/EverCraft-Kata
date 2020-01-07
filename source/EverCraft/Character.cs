36d9ca33425a803d82a788c864f8db5bb9ed5a44
Seth Jacobson <seth.jacobson@llamasoft.com>
2020-01-07 09:10:45 -0500

    Character name tests

---
+++ source/EverCraft/Character.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------------------------------------------------------------
+// <copyright file="Character.cs" company="Blizzards of the Coast">
+// © 2019, Blizzards of the Coast. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root for full license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------
+
+namespace EverCraft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Defines a character in the world.
+    /// </summary>
+    public class Character
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Character"/> class.
+        /// </summary>
+        public Character(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Gets the name of the character.
+        /// </summary>
+        public string Name { get; }
+    }
+}
---
+++ tests/EverCraft.Tests/CharacterTests.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="CharacterTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace EverCraft.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CharacterTests
+    {
+        [TestMethod]
+        public void CharacterName_CanGetName()
+        {
+            var character = new Character("Robert");
+
+            var name = character.Name;
+
+            Assert.AreEqual("Robert", name);
+        }
+
+        [TestMethod]
+        public void CharacterName_CanSetName()
+        {
+            var character = new Character("Robert");
+            character.Name = "William";
+
+            var name = character.Name;
+
+            Assert.AreEqual("William", name);
+        }
+    }
+}
